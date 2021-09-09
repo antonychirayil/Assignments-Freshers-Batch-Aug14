e4fced42912da3de390a94a0d19e13fea5f86230
antonychirayil <antonychirayil@users.noreply.github.com>
2021-09-09 17:34:01 +0530

    No change in Assign8 q8

--- src/Assignment8_Lambda/q8.java
+++ src/Assignment8_Lambda/q8.java
@@ -3,7 +3,6 @@ package Assignment8_Lambda;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
-
 public class q8 {
 
     public static void main(String[] args) {
