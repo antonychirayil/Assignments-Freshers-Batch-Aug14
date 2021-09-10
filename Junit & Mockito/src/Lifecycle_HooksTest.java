ac7cef3787081b26ccdd3372b7be603027031e3b
antonychirayil <antonychirayil@users.noreply.github.com>
2021-09-10 17:23:11 +0530

    Junit test file created for Lifecyclehooks

---
+++ Junit & Mockito/src/Lifecycle_HooksTest.java
@@ -0,0 +1,14 @@
+package src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Lifecycle_HooksTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
