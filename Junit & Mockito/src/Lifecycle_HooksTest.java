91923f754d4d4be59f57eb52a5ea1407252b632b
antonychirayil <antonychirayil@users.noreply.github.com>
2021-09-10 19:59:26 +0530

    fourth question complete

--- Junit & Mockito/src/Lifecycle_HooksTest.java
+++ Junit & Mockito/src/Lifecycle_HooksTest.java
@@ -2,6 +2,8 @@ package src;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,11 +11,29 @@ import org.junit.jupiter.api.Test;
 class Lifecycle_HooksTest {
 	
 	Lifecycle_Hooks lifecyclehooks;
+	
+	@BeforeAll
+	static void beforeAllinit() {
+		System.out.println("This works only with static method and runs only at the begining");
+	}
+	
+	@AfterAll
+	static void afterAllinit() {
+		System.out.println("This works with static method and shows at the end");
+	}
+	
 	@BeforeEach 
 	void init() {
 		lifecyclehooks = new Lifecycle_Hooks();
 	}
 
+	
+	@AfterEach
+	void cleanup() {
+		System.out.println("Cleaning up..");
+	}
+	
+	
 	@Test
 	void testAdd() {
 	
