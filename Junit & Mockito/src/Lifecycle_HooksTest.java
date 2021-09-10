f0e43a17418b4a83fff25d13649ee75fb53855b3
antonychirayil <antonychirayil@users.noreply.github.com>
2021-09-10 19:39:07 +0530

    before each added to lifecyclehooks

--- Junit & Mockito/src/Lifecycle_HooksTest.java
+++ Junit & Mockito/src/Lifecycle_HooksTest.java
@@ -2,14 +2,36 @@ package src;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Lifecycle_HooksTest {
+	
+	Lifecycle_Hooks lifecyclehooks;
+	@BeforeEach 
+	void init() {
+		lifecyclehooks = new Lifecycle_Hooks();
+	}
 
 	@Test
-	void test() {
-		Lifecycle_Hooks lifecyclehooks = new Lifecycle_Hooks();
-		assertEquals(2, lifecyclehooks.add(1, 1),"should Add two numbers");
+	void testAdd() {
+	
+		assertEquals(2, lifecyclehooks.add(1, 1),"should Add two numbers");	
+	}
+	
+	@Test
+	void testDivide() {
+		
+		assertThrows(ArithmeticException.class, () -> lifecyclehooks.divide(1, 0), "throws if it is divided by zero");
+		
+	}
+	
+	@Test
+	void testComputeCircleArea() {
+		
+		assertEquals(Math.PI, lifecyclehooks.computeCircleArea(1),"should get the cirecle area");
 	}
 
 }
+
